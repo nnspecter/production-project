7a700ecf9a3c563036101836ad90d03e36b0d44f
nnspecter <nnspecter@gmail.com>
2026-04-21 14:20:41 +0300

    feat: интеграция fsd, глобальные импорты в вебпаке и тс конфиге

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import "./styles/index.scss"
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import { Suspense, useContext, useState } from 'react'
-import { useTheme } from './theme/useTheme'
-import { classNames } from './helpers/classNames'
+import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
+import { classNames } from 'helpers/classNames'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
 
 export enum Theme {
   LIGHT = 'light',
@@ -20,8 +20,8 @@ const App = () => {
       <Link to="/about">О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-            <Route path={"/about"} element={<AboutPageAsync/>}></Route>
-            <Route path={"/"} element={<MainPageAsync/>}></Route>
+            <Route path={"/about"} element={<AboutPage/>}></Route>
+            <Route path={"/"} element={<MainPage/>}></Route>
         </Routes>
       </Suspense>
     </div>
