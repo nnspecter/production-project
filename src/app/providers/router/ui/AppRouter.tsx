2a8439d44608a556cc62e3d576339668c9546f5b
nnspecter <nnspecter@gmail.com>
2026-04-28 09:35:33 +0300

    feat: обработка ошибок через ErrorBoundary и ErrorPage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
-import { Loader } from 'shared/ui/PageLoader';
+import { Loader } from 'shared/ui/Loader';
 
 export const AppRouter = () => (
     <Suspense fallback={<Loader />}>
         <Routes>
-
             {Object.values(routeConfig).map(({ path, element }) => (
                 <Route
                     key={`path-${path}`}
